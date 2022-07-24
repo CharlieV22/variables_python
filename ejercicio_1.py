806a452e1c21307a42ac4a09dece3b012d5692fe
CharlieV22 <109441661+CharlieV22@users.noreply.github.com>
2022-07-24 17:42:37 -0300

    Add files via upload

---
+++ ejercicio_1.py
@@ -0,0 +1,35 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica
+
+numero_1 = 5
+numero_2 = 7
+
+# Realizar la suma de las dos variables
+# numero_1 y numero_2
+suma = numero_1 + numero_2
+# Almacenar el valor de la suma en una variable
+# ej: 
+numero_3 = 6
+numero_4 = 4
+
+# operacion = .....
+suma = numero_3 + numero_4
+
+# Imprimir en pantalla el resultado de la suma
+# print(....)
+print ("el resultado de la suma", suma)
+
+# Repita el procedimiento para realizar la resta
+resta = numero_1 - numero_2
+# ejemplo
+resta = numero_3 - numero_4
+
+print ("el resultado de restar", numero_1, "y", numero_2, "es", resta )
